8caffeaf0f6b0ad7ee52098508ded1d3df5a6a40
EnachescuAlin <alinenachescu033@gmail.com>
2015-11-13 22:42:59 +0200

    Included isr.h. Added extern isrFunction. Added __interrupts_memset(). Initialized interruptHandlers[] with 0.

--- src/kernel/arch/i386/interrupts/interrupts.c
+++ src/kernel/arch/i386/interrupts/interrupts.c
@@ -1,10 +1,21 @@
 #include <interrupts.h>
 #include <idt.h>
 #include <gdt.h>
+#include <isr.h>
+
+extern isrFunction interruptHandlers[];
+
+static void __interrupts_memset(char *ptr, p_cuint64 size)
+{
+    for (p_uint64 i = 0; i < size; i++)
+        ptr[i] = 0;
+}
 
 void interrupts_init(void)
 {
     idt_init();
     gdt_init();
+
+    __interrupts_memset((char*)&interruptHandlers, sizeof(isrFunction) * 256);
 }
 

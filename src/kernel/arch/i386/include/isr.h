528d6b9ed8d3923caac78e8c34f0a2aafae0b510
EnachescuAlin <alinenachescu033@gmail.com>
2015-11-13 20:02:13 +0200

    Included sys/types.h instead of common.h. Declared isr_registerInterruptHandler() and isr_handler(). Defined a pointer to a function.

--- src/kernel/arch/i386/include/isr.h
+++ src/kernel/arch/i386/include/isr.h
@@ -1,7 +1,10 @@
+/*
+ *  isr.h -- interrupt service routines
+ */
 #ifndef PhiOS_isr
 #define PhiOs_isr
 
-#include <common.h>
+#include <sys/types.h>
 
 typedef struct registers
 {
@@ -10,6 +13,12 @@ typedef struct registers
    p_u32int int_no, err_code;    // Interrupt number and error code (if applicable)
    p_u32int eip, cs, eflags, useresp, ss; // Pushed by the processor automatically.
 } Registers;
+
+typedef void (*isrFunction)(Registers);
+
+void isr_registerInterruptHandler(p_cuint8, isrFunction);
+
+void isr_handler(Registers);
 
 #endif
 

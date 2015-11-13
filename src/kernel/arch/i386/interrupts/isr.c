3cfec20569e5309339a49a23494c603e3a69d507
EnachescuAlin <alinenachescu033@gmail.com>
2015-11-13 19:59:27 +0200

    Added isr_registerInterruptHandler(). Added an isrFunction array. Modified isr_handler().

--- src/kernel/arch/i386/interrupts/isr.c
+++ src/kernel/arch/i386/interrupts/isr.c
@@ -2,11 +2,23 @@
 #include <isr.h>
 #include <vga.h>
 
-// This gets called from ASM interrupt handler stub.
+isrFunction interruptHandlers[256];
+
+void isr_registerInterruptHandler(p_cuint8 position, isrFunction function)
+{
+    interruptHandlers[position] = function;
+}
+
+/*
+ * This is called from ASM interrupt handler.
+ */
 void isr_handler(Registers regs)
 {
     vga_putString("recieved interrupt: ");
     vga_putUnsignedNumber(regs.int_no);
     vga_putChar('\n');
+
+    if (interruptHandlers[regs.int_no] != 0)
+        interruptHandlers[regs.int_no](regs);
 }
 

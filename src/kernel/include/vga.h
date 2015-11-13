ea532fe08d727c4c05aed2bd39c129b0c0ac3c13
EnachescuAlin <alinenachescu033@gmail.com>
2015-11-13 18:24:42 +0200

    Added vga.h

---
+++ src/kernel/include/vga.h
@@ -0,0 +1,41 @@
+#ifndef PhiOS_vga
+#define PhiOS_vga
+
+#include <sys/types.h>
+
+enum vga_color
+{
+    COLOR_WHITE         = 15,
+    COLOR_BLACK         = 0,
+    COLOR_BLUE          = 1,
+    COLOR_GREEN         = 2,
+    COLOR_CYAN          = 3,
+    COLOR_RED           = 4,
+    COLOR_MAGENTA       = 5,
+    COLOR_BROWN         = 6,
+    COLOR_YELLOW        = 14,
+    COLOR_DARK_GRAY     = 8,
+    COLOR_LIGHT_GRAY    = 7,
+    COLOR_LIGHT_BLUE    = 9,
+    COLOR_LIGHT_GREEN   = 10,
+    COLOR_LIGHT_CYAN    = 11,
+    COLOR_LIGHT_RED     = 12,
+    COLOR_LIGHT_MAGENTA = 13,
+};
+
+void vga_clearScreen();
+
+void vga_putChar(const char);
+
+void vga_putString(const char*);
+
+void vga_putSignedNumber(p_s32int);
+
+void vga_putUnsignedNumber(p_u32int);
+
+void vga_changeBackground(enum vga_color);
+
+void vga_changeForeground(enum vga_color);
+
+#endif
+

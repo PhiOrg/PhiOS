408efff506129f70a52bf3922be9a1391a087d97
EnachescuAlin <alinenachescu033@gmail.com>
2015-11-14 22:43:27 +0200

    Rewritten boot.

--- src/kernel/arch/i386/boot/kernel.asm
+++ src/kernel/arch/i386/boot/kernel.asm
@@ -1,22 +1,44 @@
-; Copyright (C) 2014  Arjun Sreedharan
-; License: GPL version 2 or higher http://www.gnu.org/licenses/gpl.html
-
 bits 32
+
+; multiboot header
+MB_ALIGN        equ  1<<0
+MB_MEMINFO      equ  1<<1
+MB_FLAGS        equ  MB_ALIGN | MB_MEMINFO
+MB_MAGIC        equ  0x1BADB002
+MB_CHECKSUM     equ -(MB_MAGIC + MB_FLAGS)
+
+section .multiboot
+align 4
+    dd MB_MAGIC
+    dd MB_FLAGS
+    dd MB_CHECKSUM
+
+; reserve space for kernel stack
+section .bootstrap_stack
+    align 4
+global stack_bottom
+stack_bottom:
+    times 8192 db 0
+
+global stack_top
+stack_top:
+
+; start kernel
 section .text
-        align 4
-        dd 0x1BADB002                   ;magic
-        dd 0x00                         ;flags
-        dd - (0x1BADB002 + 0x00)        ;checksum. m+f+c should be zero
+global _start
+_start:
+    extern phi_main
 
-global start
-extern phi_main                         ;this is defined in the c file
+    mov esp, stack_top ; set stack
+    push esp ; push stack pointer
+    push ebx ; push multiboot structure
 
-start:
-    cli                                 ;block interrupts
-    mov esp, stack_space                ;set stack pointer
+    cli
+
     call phi_main
-    hlt                                 ;halt the CPU
 
-section .bss
-resb 8192                               ;8KB for stack
-stack_space:
+    cli
+.hang:
+    hlt
+    jmp .hang ; stop the processor executing whatever rubbish is in the
+              ; memory after kernel

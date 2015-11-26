fd901c06d324812ca16216e2090525d7f732f4b3
EnachescuAlin <alinenachescu033@gmail.com>
2015-11-26 23:18:25 +0200

    Added a small example.

--- src/libc/phiio/phiio.c
+++ src/libc/phiio/phiio.c
@@ -3,6 +3,16 @@
 
 void printk(str) char str[];
 {
-
+    /* Now you can write the function.
+    p_uint32 *p = &str;
+    vga_putString(*p);
+    p++;
+    vga_putUnsignedNumber(*p);
+    vga_putChar('\n');
+    p++;
+    vga_putString(*p);
+    p++;
+    vga_putUnsignedNumber(*p);
+    */
 }
 

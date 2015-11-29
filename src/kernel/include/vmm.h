c2c5cff111970984fbd9e8a29edaab52ff46fbd5
EnachescuAlin <alinenachescu033@gmail.com>
2015-11-29 15:31:36 +0200

    Removed Page structure. Added the functions declarations from vmm.c. Added some defines.

--- src/kernel/include/vmm.h
+++ src/kernel/include/vmm.h
@@ -7,22 +7,18 @@
 
 #include <sys/types.h>
 
-// Page = 4kb physical memory
-typedef struct page_t
-{
-    p_uint8 present :1;  // Page present in memory
-    p_uint8 rw      :1;  // 1 for read-write, 0 read-only
-    p_uint8 user    :1;  // Supervisor level only if clear
-    p_uint8 accesed :1;  // Has the page been accessed since last refresh?
-    p_uint8 dirty   :1;  // Has the page been written since last refresh?
-    p_uint8 unused  :7;  // Amalgamation of unused and reserved bits
-    p_uint32 frame  :20; // Frame address (shifted >> 12 bits)
-} Page;
+#ifdef PhiArchi386
+#include <isr.h>
+#endif
+
+#define PAGE_PRESENT 0x1
+#define PAGE_READ_WRITE 0x2
+#define PAGE_USER_MODE 0x4
 
 // PageTable == 4mb physical memory
 typedef struct page_table_t
 {
-    Page pages[1024];
+    p_uint32 pages[1024];
 } PageTable;
 
 // PageDirectory == 4gb physical memory
@@ -32,6 +28,14 @@ typedef struct page_directory_t
     p_uint32 physicalTables[1024];  // physical addresss of the tables
     p_uint32 physicalAddress;       // the physical address of physicalTables
 } PageDirectory;
+
+void vmm_init();
+void vmm_pageFault(Registers);
+void vmm_switchPageDirectory(PageDirectory*);
+void vmm_enablePaging();
+void vmm_allocPage(p_uint32, p_uint32, PageDirectory*);
+void vmm_freePage(p_uint32, PageDirectory*);
+void vmm_allocArea(p_uint32, p_uint32, p_uint32, PageDirectory*);
 
 #endif
 

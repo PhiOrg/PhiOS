c8efbfc2ac24547929de596d373d8503cbe48536
EnachescuAlin <alinenachescu033@gmail.com>
2015-11-29 15:30:03 +0200

    Removed the commented code.

--- src/kernel/arch/i386/interrupts/isr.c
+++ src/kernel/arch/i386/interrupts/isr.c
@@ -1,6 +1,5 @@
 #include <common.h>
 #include <isr.h>
-#include <vga.h>
 
 isrFunction interruptHandlers[256];
 
@@ -14,10 +13,6 @@ void isr_registerInterruptHandler(p_cuint8 position, isrFunction function)
  */
 void isr_handler(Registers regs)
 {
-    //vga_putString("recieved interrupt: ");
-    //vga_putUnsignedNumber(regs.int_no);
-    //vga_putChar('\n');
-
     if (interruptHandlers[regs.int_no] != 0)
         interruptHandlers[regs.int_no](regs);
 }

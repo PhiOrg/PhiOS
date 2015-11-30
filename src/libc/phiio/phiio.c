16e8f9932a549fff1c8386f6f1e2e2fa8d8448f5
Mega Watt <megawatt@openmailbox.org>
2015-11-30 22:54:07 +0200

    printk ver 0.2

--- src/libc/phiio/phiio.c
+++ src/libc/phiio/phiio.c
@@ -1,57 +1,51 @@
 #include <phiio.h>
 #include <vga.h>
+#include <phistr.h>
 
 
 void printk(str) char str[];
 {
 
-  /* Now you can write the function.
-     p_uint32 *p = &str;
-     vga_putString(*p);
-     p++;
-     vga_putUnsignedNumber(*p);
-     vga_putChar('\n');
-     p++;
-     vga_putString(*p);
-     p++;
-     vga_putUnsignedNumber(*p);
-  */
 
   p_uint32 *p = &str;
-  char *s = *p;
  
-  int strlen = phistr_len(s); // Optimization: inlining the function call
+  int strlen = phistr_len(str);
   p++;
-  int i;
+  p_uint32 i;
   for (i = 1; i < strlen; i++) {
-    // Obvious optimizations: storing s[i] in a variable
-    if (s[i - 1] == '%' && (s[i] == 'p' || s[i] == 's' || s[i] == 'c' || s[i] == 'd' || s[i] == 'u')) {
-      switch(s[i]) {
+
+    if (str[i - 1] == '%' && (str[i] == 'p'
+                           || str[i] == 's'
+                           || str[i] == 'c'
+                           || str[i] == 'd'
+                           || str[i] == 'u')) {
+      switch (str[i]) {
       case 's':
-	vga_putString(*p);
-	p++;
-	break;
+        	vga_putString(*p);
+        	p++;
+        	break;
       case 'd':
-	vga_putSignedNumber(*p);
-	p++;
-	break;
-    case 'u':
-  vga_putUnsignedNumber(*p);
-  p++;
-  break;
+        	vga_putSignedNumber(*p);
+        	p++;
+        	break;
+      case 'u':
+          vga_putUnsignedNumber(*p);
+          p++;
+          break;
       case 'c':
-	vga_putChar(*p);
-	p++;
-	break;
+        	vga_putChar(*p);
+        	p++;
+        	break;
       case 'p':
-	vga_putAddress(*p, ' ', 1); // TODO: modify
-	p++;
-	break;
+        	vga_putAddress(*p, 1, 1);
+        	p++;
+        	break;
       }
+
       i++;
     }
     else
-      vga_putChar(s[i - 1]);
+      vga_putChar(str[i - 1]);
   }
 }
 

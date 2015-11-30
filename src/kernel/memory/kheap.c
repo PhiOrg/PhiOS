766ed620d6c21e545c73645864a08226a579d4b8
EnachescuAlin <alinenachescu033@gmail.com>
2015-11-30 19:26:19 +0200

    Added kheap_mallocPageTable_init() and kheap_mallocPageTable().

--- src/kernel/memory/kheap.c
+++ src/kernel/memory/kheap.c
@@ -2,6 +2,8 @@
 
 extern p_size_t kernel_end;
 p_size_t kheap_placementAddress = (p_size_t) &kernel_end;
+p_size_t kheap_mallocPageTable_placementAddress;
+p_size_t kheap_mallocPageTable_maxAddress;
 
 static p_size_t __kheap_kmalloc(p_size_t size, p_uint8 align, p_size_t *phys)
 {
@@ -40,5 +42,22 @@ p_size_t kheap_kmalloc_ap(p_size_t size, p_size_t *phys)
 p_size_t kheap_kmalloc(p_size_t size)
 {
     return __kheap_kmalloc(size, 0, 0);
+}
+
+p_size_t kheap_mallocPageTable(p_size_t size, p_size_t *phys)
+{
+    p_size_t tmp = kheap_mallocPageTable_placementAddress;
+    *phys = kheap_mallocPageTable_placementAddress;
+    kheap_mallocPageTable_placementAddress += size;
+
+    return tmp;
+}
+
+void kheap_mallocPageTable_init(p_size_t size)
+{
+    kheap_mallocPageTable_placementAddress = (kheap_placementAddress & 0xFFFFF000) + 0x1000;
+    kheap_mallocPageTable_maxAddress = kheap_mallocPageTable_placementAddress +
+                                       1024 * size;
+    kheap_placementAddress = kheap_mallocPageTable_maxAddress;
 }
 

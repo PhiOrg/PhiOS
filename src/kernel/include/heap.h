4a41c16bad3eb6c8dacb0973a9c12f4b19d9a74c
EnachescuAlin <alinenachescu033@gmail.com>
2015-12-10 15:40:24 +0200

    Created file and defined the Heap structure. Added heap_create() and heap_malloc().

---
+++ src/kernel/include/heap.h
@@ -0,0 +1,18 @@
+#ifndef PhiOS_heap
+#define PhiOS_heap
+
+#include <sys/types.h>
+#include <list.h>
+#include <vmm.h>
+
+typedef struct heap
+{
+    p_size_t size, used;
+    List *head;
+} Heap;
+
+Heap* heap_create(PageDirectory*);
+void* heap_malloc(Heap*, p_size_t);
+
+#endif
+
